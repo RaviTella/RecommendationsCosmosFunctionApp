4f46cbe76859f9e6d258d0e3d4d24acb7f6f7582
Ravi Tella <RaviTella@users.noreply.github.com>
2021-05-24 14:01:30 -0500

    uncomment

--- RecommendationsCosmosFunction.cs
+++ RecommendationsCosmosFunction.cs
@@ -30,12 +30,11 @@ namespace RecommendationsCosmosFunctionApp
         public class Recommendation
         {
             public string Id { get; set; }
-            /*            public string Isbn { get; set; }
-                        public string Title { get; set; }
-                        public string Author { get; set; }
-                        public string Description { get; set; }
-                        public string ImageURL { get; set; }
-            */
+            public string Isbn { get; set; }
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public string Description { get; set; }
+            public string ImageURL { get; set; }
         }
     }
 }
